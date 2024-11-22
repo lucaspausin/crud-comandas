b96bb7d0f458cb5ccdc32060acac5a64d41b0ce9
@ASTROboy3 <lucaspausin@gmail.com>
2024-11-22 13:50:21 -0300

    dd

--- frontend/src/components/commands/TechnicalDetailsCard.jsx
+++ frontend/src/components/commands/TechnicalDetailsCard.jsx
@@ -1,14 +1,18 @@
 import { useState, lazy, useEffect, useRef } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
+import DownloadIcon from "@/components/DownloadIcon";
 
 // Lazy load the VehicleViewerReadOnly component
-const VehicleViewerReadOnly = lazy(() => import("@/components/3D/VehicleViewerReadOnly"));
+const VehicleViewerReadOnly = lazy(() =>
+	import("@/components/3D/VehicleViewerReadOnly")
+);
 
 export default function TechnicalDetailsCard({ comanda }) {
 	const [selectedPoint, setSelectedPoint] = useState(null);
 	const [isViewerVisible, setIsViewerVisible] = useState(false); // State to track visibility
 	const viewerRef = useRef(null);
+	const slidesRef = useRef(null); // Ref para el contenido a descargar como PDF
 
 	// Extraemos los detalles técnicos de la comanda
 	const detallesTecnicos = comanda?.tecnica_tecnica_comanda_idTocomandas || {};
@@ -51,6 +55,24 @@ export default function TechnicalDetailsCard({ comanda }) {
 		};
 	}, []);
 
+	const handleGeneratePdf = async () => {
+		if (typeof window !== "undefined") {
+			const html2pdf = (await import("html2pdf.js")).default;
+			const opt = {
+				margin: 0.5,
+				filename: `Inspección del Vehículo - ${detallesTecnicos.marca_vehiculo}.pdf`,
+				image: { type: "jpeg", quality: 1 },
+				html2canvas: { scale: 2, useCORS: true },
+				jsPDF: {
+					unit: "in",
+					format: "letter",
+					orientation: "portrait",
+				},
+			};
+			html2pdf().from(slidesRef.current).set(opt).save();
+		}
+	};
+
 	if (hasEmptyValues) {
 		return (
 			<Card className="border-none shadow-lg rounded-lg h-full col-span-2">
@@ -73,12 +95,13 @@ export default function TechnicalDetailsCard({ comanda }) {
 	return (
 		<>
 			<Card className="col-span-2 border-none shadow-lg rounded-lg">
-				<CardHeader className="p-6">
+				<CardHeader className="p-6 flex flex-row items-center justify-between">
 					<CardTitle className="text-xl font-light text-zinc-800">
 						Inspección del Vehículo
 					</CardTitle>
+					<DownloadIcon onClick={handleGeneratePdf} label="Descargar" />
 				</CardHeader>
-				<CardContent className="py-0 px-6">
+				<CardContent className="py-0 px-6" ref={slidesRef}>
 					<dl className="grid grid-cols-4 gap-2 text-sm">
 						<dt className="font-normal text-zinc-600 mb-1">
 							Marca del Vehículo:
@@ -121,6 +144,38 @@ export default function TechnicalDetailsCard({ comanda }) {
 						<dd className="text-zinc-800">
 							{detallesTecnicos.observaciones_tecnicas || "N/A"}
 						</dd>
+					</dl>
+
+					<dl className="grid grid-cols-[auto,auto,auto,auto] lg:grid-cols-4 gap-2 text-sm">
+						{Object.keys(detallesTecnicos)
+							.filter(
+								(key) => key.startsWith("detalle") && !isNaN(key.slice(7))
+							)
+							.filter(
+								(key) =>
+									detallesTecnicos[key] !== null && detallesTecnicos[key] !== ""
+							)
+							.filter((key, index) => index < 20)
+							.map((key) => (
+								<div key={key} className="contents">
+									<dt className="text-zinc-600 font-normal">
+										Detalle {key.slice(7)}:
+									</dt>
+									<dd>{detallesTecnicos[key]}</dd>
+								</div>
+							))}
+						<dt className="font-normal text-zinc-600 mb-2">
+							Firma del Cliente:
+						</dt>
+						{detallesTecnicos.firma && (
+							<Image
+								src={`data:image/png;base64,${detallesTecnicos.firma}`}
+								alt="Firma"
+								width={300}
+								height={100}
+								className="border-none rounded-lg"
+							/>
+						)}
 					</dl>
 				</CardContent>
 				<CardContent className="col-span-2 border-none rounded-lg p-6">
@@ -175,39 +230,6 @@ export default function TechnicalDetailsCard({ comanda }) {
 							)}
 						</div>
 					</div>
-				</CardContent>
-				<CardContent>
-					<dl className="grid grid-cols-[auto,auto,auto,auto] lg:grid-cols-4 gap-2 text-sm">
-						{Object.keys(detallesTecnicos)
-							.filter(
-								(key) => key.startsWith("detalle") && !isNaN(key.slice(7))
-							)
-							.filter(
-								(key) =>
-									detallesTecnicos[key] !== null && detallesTecnicos[key] !== ""
-							)
-							.filter((key, index) => index < 20)
-							.map((key) => (
-								<div key={key} className="contents">
-									<dt className="text-zinc-600 font-normal">
-										Detalle {key.slice(7)}:
-									</dt>
-									<dd>{detallesTecnicos[key]}</dd>
-								</div>
-							))}
-						<dt className="font-normal text-zinc-600 mb-2">
-							Firma del Cliente:
-						</dt>
-						{detallesTecnicos.firma && (
-							<Image
-								src={`data:image/png;base64,${detallesTecnicos.firma}`}
-								alt="Firma"
-								width={300}
-								height={100}
-								className="border-none rounded-lg"
-							/>
-						)}
-					</dl>
 				</CardContent>
 			</Card>
 		</>
