3222027ee48c1fa9ebb5c79a9c3d959511c206bf
lucaspausin <lucaspausin@gmail.com>
2025-02-14 22:09:34 -0300

    1.3.6a

--- frontend/src/app/settings/customers/page.jsx
+++ frontend/src/app/settings/customers/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import Aside from "@/components/Aside";
 import api from "@/lib/axios";
 import {
